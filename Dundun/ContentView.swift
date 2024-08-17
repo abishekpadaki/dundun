7b0581da42e2ad165af0305e949b2f68c37ea5bb
Abishek Padaki <abiks192@gmail.com>
2024-08-16 20:34:17 -0700

    Initial Commit

---
+++ Dundun/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Dundun
+//
+//  Created by Abishek Padaki on 8/16/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Dundun/DundunApp.swift
@@ -0,0 +1,17 @@
+//
+//  DundunApp.swift
+//  Dundun
+//
+//  Created by Abishek Padaki on 8/16/24.
+//
+
+import SwiftUI
+
+@main
+struct DundunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
